aa082bf384b79cf66a1dc674b31b1a59c66c36ef
M.Soroush Zamzam <57797838+soroshzzz26@users.noreply.github.com>
2024-10-03 13:28:39 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TextStyle} from 'react-native';
 import {findAll} from 'highlight-words-core';
 import PropTypes from 'prop-types';
 
 Highlighter.propTypes = {
     autoEscape: PropTypes.bool,
-    highlightStyle: Text.propTypes.style,
+    highlightStyle: TextStyle,
     searchWords: PropTypes.arrayOf(PropTypes.string).isRequired,
     textToHighlight: PropTypes.string.isRequired,
     sanitize: PropTypes.func,
-    style: Text.propTypes.style
+    style: TextStyle
 };
 
 /**
